154a2fbad81e437aea629d9a1e95484bb9374fce
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-04-28 09:42:46 -0700

    add README and repo links to cargo

--- examples/tags.rs
+++ examples/tags.rs
@@ -2,6 +2,6 @@
 #[macro_use] extern crate rdxl;
 
 fn main(){
-   println!("{}", rdxl!(<div>dave</div>) );
-   println!("{}", rdxl!(<a href="there">this that</a><br><p attr="something.f()">that this</p>) );
+   //println!("{}", rdxl!(<div>dave</div>) );
+   //println!("{}", rdxl!(<a href="there">this that</a><br><p attr="something.f()">that this</p>) );
 }
