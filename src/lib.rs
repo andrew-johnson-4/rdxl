84f0c07cadb995ebba0924b3942ac0d0f4d0a4c0
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-05-23 19:51:42 -0700

    Add some crate level documentation and bump to version 0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,61 @@
+// Copyright 2020, The rdxl Project Developers.
+// Dual Licensed under the MIT license and the Apache 2.0 license,
+// see the LICENSE file or <http://opensource.org/licenses/MIT>
+// also see LICENSE2 file or <https://www.apache.org/licenses/LICENSE-2.0>
+
+//! # HTML Templating Macros
+//!
+//! This library contains JSX style inline HTML macros for Rust. The only
+//! dependencies are the ubiquitous proc-macro2, syn, and quote.
+//!
+//! The library is semantic versioned, so expect small fixes and many additions until a 1.0 release is reached.
+//!
+//! # Inline Rust expressions
+//!
+//! ```no_run
+//! let x = 5;
+//! let y = "asdf";
+//! rdxl!({{ x }}, {{ y }})
+//! ```
+//!
+//! # Conditional expressions
+//!
+//! ```no_run
+//! let x = 5;
+//! let y = "asdf";
+//! rdxl!({{ if x>3 {{
+//!   Case One
+//! }} else if x>2 {{
+//!   Case Two
+//! }} else {{
+//!   Case Three
+//! }} }})
+//! ```
+//!
+//! # Loop expressions
+//!
+//! ```no_run
+//! rdxl!(<ul>{{ for i in 0..10 {{
+//!   <li>{{ i }}</li>
+//! }} }}</ul>)
+//! ```
+//!
+//! # Miscellaneous expressions
+//!
+//! ```no_run
+//! rdxl!(
+//!   {{ let x = 5; }}
+//!   {{ x }}
+//! )
+//! ```
+//!
+//! # Html attributes
+//! ```no_run
+//! rdxl(<div style={{ "\"color:red;\"" }}>
+//!   inside div
+//! </div>)
+//! ```
+
 #![recursion_limit = "128"]
 #![feature(type_ascription)]
 #![crate_type = "proc-macro"]
@@ -8,7 +66,6 @@ use proc_macro2::{Spacing, Span, Punct, Literal, Ident, Group, Delimiter};
 use syn::parse::{Parse, ParseStream, Result, Error};
 use syn::{parse_macro_input, Ident as SynIdent, Token, Expr, Pat, LitStr, bracketed, braced};
 use syn::token::{Bracket,Brace};
-
 
 struct RdxlExprF {
    context: String,
