17c36ebb80968824c43bf771e3acd7f8ea49c585
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-03 11:55:22 -0700

    tag tests all passing

--- tests/tags.rs
+++ tests/tags.rs
@@ -1,9 +1,45 @@
 #![feature(proc_macro_hygiene)]
 use rdxl::xhtml;
 
-fn main(){
-   println!("{}", xhtml!(<div>dave</div><div>david</div>) );
-   println!("{}", xhtml!(<a href="there">this that</a><br/><p attr="something.f()">that this</p>) );
-   println!("{}", xhtml!(<input type="text"/>))
-   //println!("{}", xhtml!(<a></b>) );
+#[test]
+fn tag1() {
+   assert_eq!(
+      xhtml!("<div></div>"),
+      "<div></div>".to_string()
+   );
 }
+
+#[test]
+fn tag2() {
+   assert_eq!(
+      xhtml!(<div>dave</div><div>david</div>),
+      "<div>dave</div><div>david</div>".to_string()
+   );
+}
+
+#[test]
+fn tag3() {
+   assert_eq!(
+      xhtml!(<a href="there">this that</a><br/><p attr="something.f()">that this</p>),
+      "<a href=\"there\">this that</a><br/><p attr=\"something.f()\">that this</p>".to_string()
+   );
+}
+
+#[test]
+fn tag4() {
+   assert_eq!(
+      xhtml!(<input type="text"/>),
+      "<input type=\"text\"/>".to_string()
+   );
+}
+
+/* panics during compilation, not test; so I don't know how to unit test this
+#[test]
+#[should_panic]
+fn tag5() {
+   assert_eq!(
+      xhtml!(<a></b>),
+      "<a></b>".to_string()
+   );
+}
+*/
