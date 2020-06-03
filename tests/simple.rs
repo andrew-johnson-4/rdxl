db4b43bc4371659ccac9811ce6535ca67db1b09b
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-03 11:47:20 -0700

    move one example to test format

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,10 +1,42 @@
 #![feature(proc_macro_hygiene)]
 use rdxl::xhtml;
 
-fn main(){
-   println!("{}", xhtml!(World) );
-   println!("{}", xhtml!(Hello World) );
-   println!("{}", xhtml!(Hello World ,.!@$#%^*|:;,.?/~) );
-   println!("{}", xhtml!(while if let continue break) );
-   println!("{}", xhtml!("#""{""}#"));
+#[test]
+fn test_fragment1(){
+   assert_eq!(
+      xhtml!(World),
+      "World".to_string()
+   );
+}
+
+#[test]
+fn test_fragment2(){
+   assert_eq!(
+      xhtml!(Hello World),
+      "Hello World".to_string()
+   );
+}
+
+#[test]
+fn test_fragment3(){
+   assert_eq!(
+      xhtml!(Hello World ,.!@$#%^*|:;,.?/~),
+      "Hello World ,.!@$#%^*|:;,.?/~".to_string()
+   );
+}
+
+#[test]
+fn test_fragment4(){
+   assert_eq!(
+      xhtml!(while if let continue break),
+      "while if let continue break".to_string()
+   );
+}
+
+#[test]
+fn test_fragment5(){
+   assert_eq!(
+      xhtml!("#""{""}#"),
+      "#{}#".to_string()
+   );
 }
