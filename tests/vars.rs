f804bec4f44ffee4f24a71042799c050a9234e59
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-03 12:11:56 -0700

    another test passing

--- tests/vars.rs
+++ tests/vars.rs
@@ -12,11 +12,29 @@ impl fmt::Display for MyStruct {
     }
 }
 
-fn main(){
+#[test]
+fn vars1(){
    let my_int = 22;
+   assert_eq!(
+      xhtml!(<div>{{ my_int }}</div>),
+      "<div>22</div>".to_string()
+   );
+}
+
+#[test]
+fn vars2() {
    let my_str = "ndklasfjkli";
+   assert_eq!(
+      xhtml!(<div>{{ my_str }}</div>),
+      "<div>ndklasfjkli</div>".to_string()
+   );
+}
+
+#[test]
+fn vars3() {
    let my_struct = MyStruct { a:1, b:2 };
-   println!("{}", xhtml!(<div>{{ my_int }}</div>) );
-   println!("{}", xhtml!(<div>{{ my_str }}</div>) );
-   println!("{}", xhtml!(<div>{{ my_struct }}</div>) );
+   assert_eq!(
+      xhtml!(<div>{{ my_struct }}</div>),
+      "<div>MyStruct{a:1, b:2}</div>".to_string()
+   );
 }
