8ed34a297cb0150d18d33e21f878fc4770855e54
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-24 15:50:59 -0700

    Default::default() tests are passing

--- tests/class_defaults.rs
+++ tests/class_defaults.rs
@@ -1,11 +1,7 @@
 use rdxl::{xhtml,xtype,xrender};
 
 xtype!(<!A b:String c:u64/>);
-xtype!(<!B a:bool c:u64/>);
-xtype!(<!C a:bool b:String/>);
 xrender!(A,{{self.b}}{{self.c}});
-xrender!(B,{{self.a}}{{self.c}});
-xrender!(C,{{self.a}}{{self.b}});
 
 #[test]
 fn default1() {
