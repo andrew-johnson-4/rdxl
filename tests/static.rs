c063982b17a1194c11af077a75d3cf1835904f76
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-28 15:34:09 -0700

    add test case for lifetimes in nested contexts and moves

---
+++ tests/static.rs
@@ -0,0 +1,23 @@
+#![feature(proc_macro_hygiene)]
+use rdxl::{xtype,xrender,xhtml};
+
+xtype!(<!A><?/></A>);
+xrender!(A,<br/>);
+
+#[test]
+fn static1(){
+   fn a(x: String) -> String {
+      xhtml!(<!A><?>{{ x }}</?></A>)
+   }
+   assert_eq!(a("abc".to_string()), "<br/>");
+}
+
+/* should panic? because it does rn
+#[test]
+fn static2(){
+   fn a(x: &str) -> String {
+      xhtml!(<!A><?>{{ x }}</?></A>)
+   }
+   assert_eq!(a("abc"), "<br/>");
+}
+*/
