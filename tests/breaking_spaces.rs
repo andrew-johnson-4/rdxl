05c626c35594a9092ce0775ea5db9a5f868dddfe
Andrew Johnson <andrew@subarctic.org>
2021-01-13 15:41:07 -0700

    fix xtext to never detect and insert lexed spaces

--- tests/breaking_spaces.rs
+++ tests/breaking_spaces.rs
@@ -1,4 +1,4 @@
-use rdxl::xhtml;
+use rdxl::{xhtml,xtext};
 
 fn bs(s: String) -> String {
    s.split_whitespace().collect::<Vec<&str>>().join(" ")
@@ -17,6 +17,19 @@ fn breaking_for() {
       "<ul> <li>1</li> <li>2</li> <li>3</li> <li>4</li> </ul>"
    );
 }
+#[test]
+fn nonbreaking_for() {
+   assert_eq!(
+      &xtext!(<ul>
+        <li>1</li>
+        <li>{{ 2 }}</li>
+        {{ for i in 3..5 {{
+          <li>{{ i }}</li>
+        }} }}
+      </ul>),
+      "<ul><li>1</li><li>2</li><li>3</li><li>4</li></ul>"
+   );
+}
 
 #[test]
 fn breaking_while() {
@@ -29,6 +42,19 @@ fn breaking_while() {
         }} }}
       </ul>)),
       "<ul> <li>3</li> <li>2</li> <li>1</li> </ul>"
+   );
+}
+#[test]
+fn nonbreaking_while() {
+   assert_eq!(
+      &xtext!(<ul>
+        {{ let mut i = 3; }}
+        {{ while i>0 {{
+          <li>{{ i }}</li>
+          {{ i -= 1; }}
+        }} }}
+      </ul>),
+      "<ul><li>3</li><li>2</li><li>1</li></ul>"
    );
 }
 
@@ -52,6 +78,28 @@ fn breaking_if() {
         }} }}
       </ul>)),
       "<ul> <li>1</li> <li>2</li> <li>3</li> </ul>"
+   );
+}
+#[test]
+fn nonbreaking_if() {
+   assert_eq!(
+      &xtext!(<ul>
+        {{ let x = 5; }}
+        {{ if x>4 {{
+          <li>1</li>
+        }} }}
+        {{ if x<4 {{
+          <li>1</li>
+        }} else if x>4 {{
+          <li>2</li>
+        }} }}
+        {{ if x<4 {{
+          <li>1</li>
+        }} else {{
+          <li>3</li>
+        }} }}
+      </ul>),
+      "<ul><li>1</li><li>2</li><li>3</li></ul>"
    );
 }
 
